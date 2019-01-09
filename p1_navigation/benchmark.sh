83370c2dae5cc8fc833f33516faca4b9614c449e
klee <ulamaca.lee@gmail.com>
2019-01-09 10:47:37 +0800

    ADD: benchmark scripts for experiment replications

---
+++ p1_navigation/benchmark.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# check for the exps to run
+for i in {1..2}; do
+    echo python run.py -n "duddqn-$i" -dd -du -ra "$i"
+    echo python run.py -n "dddqn-$i" -dd -ra "$i"
+    echo python run.py -n "dudqn-$i" -du -ra "$i"
+    echo python run.py -n "dqn-$i" -ra "$i"
+done
+
+# real run
+for i in {1..5}; do
+    python run.py -n "eduddqn-$i" -dd -du -ra "$i"
+    python run.py -n "edddqn-$i" -dd -ra "$i"
+    python run.py -n "edudqn-$i" -du -ra "$i"
+    python run.py -n "edqn-$i" -ra "$i"
+done
